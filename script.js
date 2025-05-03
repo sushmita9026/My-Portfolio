edc84a8dd00947ca30bfd7938bd40fd994a8815e
sushmita9026 <sushmitajnv2005@gmail.com>
2025-05-03 16:08:41 +0530

    correction

--- script.js
+++ script.js
@@ -11,21 +11,17 @@ let form = document.querySelector("form");
       submitButton.addEventListener("click", (e) => {
         e.preventDefault();
         let data = new FormData(form);
-
-        // Make the fetch request to the Google Apps Script URL
         fetch("https://script.google.com/macros/s/AKfycbwPrBCbmJl3ofllIe3MdVdzEt8WC1OkMV1tr8iSZV-Se7z7JTgri-6dbqTaVAl9yQaI/exec", {
           method: "POST",
           body: data,
         })
           .then((res) => res.text())
           .then((data) => {
-            // Show the success or failure message in #msg
             document.querySelector("#msg").innerHTML = "Message sent successfully!";
-            // Optionally, reset the form after submission
+            
             form.reset();
           })
           .catch((error) => {
-            // Handle any errors that occur during the fetch
             document.querySelector("#msg").innerHTML = "Error sending message: " + error.message;
           });
       });
